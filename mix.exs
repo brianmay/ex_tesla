3b97523d332a440c5c42389315f217108dc9baf3
Brian May <brian@linuxpenguins.xyz>
2018-11-05 12:29:39 +1100

    Remove trailing comma

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ExTesla.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger]
     ]
   end
 
